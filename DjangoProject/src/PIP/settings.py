5690a3126d671064020e98aa84dfab77a40524dc
zaira <pipdelivery9@gmail.com>
2024-02-24 16:50:56 +0100

    Configuracion Django-MySql

--- DjangoProject/src/PIP/settings.py
+++ DjangoProject/src/PIP/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PIP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PIP',
+        'USER': 'zaira',
+        'PASSWORD': 'Abc123.',
+        'HOST':'localhost',
+        'PORT': '3306'
     }
 }
 
